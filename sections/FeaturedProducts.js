fd53c0002fa277da017059a59d09fe92b310e58e
Landi <lizanderbarjamaj@yahoo.com>
2024-09-28 16:45:43 +0200

    changes

--- sections/FeaturedProducts.js
+++ sections/FeaturedProducts.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FeaturedProductsData } from '../data/FeaturedProductsData';
 import Image from 'next/image';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import '../app/globals.css'; // Ensure this is correctly imported
 
 const FeaturedProducts = () => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
   const settings = {
     dots: true,
     infinite: true,
@@ -14,7 +15,10 @@ const FeaturedProducts = () => {
     slidesToShow: 3,
     slidesToScroll: 1,
     appendDots: dots => (
-      <ul className="custom-dots"> {dots} </ul>
+      <ul className="custom-dots"> {dots.slice(0, 4)} </ul>
+    ),
+    customPaging: (i) => (
+      <div className="custom-dot w-5 m-3 h-1 mx-1 bg-pink-600"></div>
     ),
     responsive: [
       {
@@ -45,13 +49,19 @@ const FeaturedProducts = () => {
         <Slider {...settings}>
           {FeaturedProductsData.map((product, index) => (
             <div key={index} className="p-4">
-              <div className="border rounded-2xl shadow-lg max-w-xs mx-auto flex flex-col items-center hover:border-4 hover:border-solid hover:border-blue-900"
-               style={{ backgroundColor: 'rgb(242, 240, 248)' }}>
+              <div
+                className={`border rounded-2xl shadow-lg max-w-xs mx-auto flex flex-col items-center transition-transform duration-300 ${
+                  activeIndex === index ? 'border-4 border-blue-900 transform scale-105' : 'border'
+                }`}
+                style={{ backgroundColor: 'rgb(242, 240, 248)' }}
+                onMouseEnter={() => setActiveIndex(index)}
+                onClick={() => setActiveIndex(index)}
+              >
                 <Image src={product.image} alt={product.title} className="w-full h-48 object-contain" />
                 <div className='flex flex-col justify-center items-center text-justify'>
-                  <h2 className="text-xl font-bold mt-2 text-pink-600 p-1">{product.title}</h2>
-                  <p className="text-gray-600 p-1">Code: {product.code}</p>
-                  <p className="text-gray-800 font-semibold p-1">Price: ${product.price}</p>
+                  <h2 className="text-xl font-bold mt-2 text-pink-600 p-1 transition-transform duration-300 hover:scale-105">{product.title}</h2>
+                  <p className="text-gray-600 p-1 transition-transform duration-300 hover:scale-105">Code: {product.code}</p>
+                  <p className="text-gray-800 font-semibold p-1 transition-transform duration-300 hover:scale-105">Price: ${product.price}</p>
                 </div>
               </div>
             </div>
